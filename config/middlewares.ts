ef3862168421ad13168691c099189d87cc6c3e2e
sido200 <sidali20012017@outlook.com>
2025-10-09 14:37:48 +0100

    fix cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://82.29.169.69:1337', // your API (optional)
+        'http://82.29.169.69', // if you access frontend by IP
+        'http://localhost:5173', // if you test locally with Vite
+        'https://your-frontend-domain.com', // replace with your frontend URL
+      ],
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
