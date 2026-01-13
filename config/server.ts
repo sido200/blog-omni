bd1c043949be3207df6c4e5c619bb1b4fab80b57
berchiche willem <berwillempro@gmail.com>
2026-01-13 02:27:12 +0100

    adding proxy reclaim

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  host: "0.0.0.0",
+  port: env.int("PORT", 1337),
+  url: "https://api-omni.codeandsens.com",
+  proxy: true,
 });
