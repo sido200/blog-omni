1e94308e7ff50b61bf7fa4569daba232472e33e3
berchiche willem <berwillempro@gmail.com>
2026-01-14 13:16:40 +0100

    strapi cors disabeled

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,17 +1,17 @@
 export default [
   "strapi::errors",
+
   {
     name: "strapi::cors",
     config: {
-      enabled: true,
-      origin: ["https://omnicanal-dz.com", "https://www.omnicanal-dz.com"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      credentials: true,
+      enabled: false,
     },
   },
+
   "strapi::security",
-  "strapi::body",
   "strapi::query",
+  "strapi::body",
   "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
